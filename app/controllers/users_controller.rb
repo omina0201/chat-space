c13ccfce0d28a1a0b5bc036719be0e9597e05f4e
omina109 <o.m.3.31.2019@gmail.com>
2020-07-12 23:54:31 +0900

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def index
+    @users = User.all
+  end
+  
   def update
     if current_user.update(user_params)
       redirect_to root_path
