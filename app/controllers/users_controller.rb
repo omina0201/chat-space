f177ff02e46b15066f73ab15a9be7c41785168ef
omina0201 <67461101+omina0201@users.noreply.github.com>
2020-07-18 18:01:07 +0900

    Revert "Update users_controller.rb"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
 
   def index
-    return nil if params[:keyword] == ""
+    @users = User.find(session[:user_id])
+    end
+
+    respond_to do |format|
+      format.html
+      format.json
+    end
+
+      return nil if params[:keyword] == ""
       @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
       respond_to do |format|
         format.html
@@ -9,7 +17,7 @@ class UsersController < ApplicationController
     end
   end
 
-  
+  end
   
   def update
     if current_user.update(user_params)
